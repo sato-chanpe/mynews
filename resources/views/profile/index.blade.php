7ace084244d94770e18708b98297a7688f4e2018
satochanpe <satoshi.muko@gmail.com>
2022-01-04 15:09:24 +0000

    PHP/Laravel 19終了

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.front')
+
+@section('content')
+    <div class="container">
+        <hr color="#c0c0c0">
+        @if (!is_null($headline)) {{-- is_null:nullであればtrue、それ以外であればfalseを返す ->$headlineが空なら飛ばして（実行しない）、データがあれば実行する --}}
+            <div class="row">
+                <div class="headline col-md-10 mx-auto">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="caption mx-auto">
+                                <div class="image">
+                                    @if ($headline->image_path)
+                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
+                                    @endif
+                                </div>
+                                <div class="title p-2">
+                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <hr color="#c0c0c0">
+        <div class="row">
+            <div class="posts col-md-8 mx-auto mt-3">
+                @foreach($posts as $post)
+                    <div class="post">
+                        <div class="row">
+                            <div class="text col-md-6">
+                                <div class="date">
+                                    {{ $post->updated_at->format('Y年m月d日') }}
+                                </div>
+                                <div class="name">
+                                    {{ str_limit($post->title, 30) }}
+                                </div>
+                                <div class="gender mt-3">
+                                    {{ str_limit($post->body, 10) }}
+                                </div>
+                                <div class="hobby mt-3">
+                                    {{ str_limit($post->body, 500) }}
+                                </div>
+                                <div class="introduction mt-3">
+                                    {{ str_limit($post->body, 1500) }}
+                                </div>
+                            </div>
+                            <div class="image col-md-6 text-right mt-4">
+                                @if ($post->image_path)
+                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <hr color="#c0c0c0">
+                @endforeach
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
