37614c4414fa605884e03065c3afbd1331f3a991
satochanpe <satoshi.muko@gmail.com>
2022-01-05 17:40:44 +0000

    Heroku課題2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <p>試しにやってみた</p>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
